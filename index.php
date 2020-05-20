c0b55ec3fa51f10dff8a9ca10341411e09af8ee5
Jovanny Leobardo Salas <jovannyleobardo10@gmail.com>
2020-05-20 01:17:50 -0500

    Practicando con git y github

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Principal</title>
+	<title>Index</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="Libreria/css/bootstrap.min.css">
 	<script type="text/javascript" src="Libreria/js/jquery-3.5.0.min.js"></script>
